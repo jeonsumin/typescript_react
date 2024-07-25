bce2462c960006fd1047a343ec3f1655cb56046c
jeonsumin <soomis7@naver.com>
2024-07-25 13:43:25 +0900

    fix import path alias settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import vitePluginSvgr from 'vite-plugin-svgr';
+import tsconfigpaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vitePluginSvgr(),
+    tsconfigpaths(),
+  ],
+  server:{
+    port:3000,
+  },
+  build:{
+    minify:'terser',
+    terserOptions:{
+      compress:{
+        drop_console: true,
+        drop_debugger: true,
+      }
+    }
+  },
 });
