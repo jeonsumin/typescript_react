80a0c6d3f2e3e7dfa63eb4c4735afab8df6c84fa
jeonsumin <soomis7@naver.com>
2024-07-25 16:49:14 +0900

    add axios

---
+++ src/plugins/axios/index.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { getCookie, setCookie } from 'plugins/cookies';
+import { redirect, useLocation } from 'react-router-dom';
+
+const path = useLocation().pathname;
+axios.create({
+  baseURL: process.env.VITE_APP_API_URL,
+});
+
+axios.interceptors.request.use((config) => {
+  const token = getCookie('token');
+  config.headers.Authorization = !!token ? `Bearer ${token}` : '';
+  return config;
+});
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401) redirect('/login');
+    else if (error.response.status === 403) {
+      if (path === '/login') {
+        return;
+      } else {
+        setCookie({ key: 'LOGIN_REDIE', value: path });
+        return redirect('/login');
+      }
+    }
+    return error;
+  }
+);
--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -1,4 +1,12 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+
 export default function Main() {
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/v1.0/member').then((response) => {
+      console.log(response);
+    });
+  }, []);
   return (
     <>
       <h1>Main</h1>
