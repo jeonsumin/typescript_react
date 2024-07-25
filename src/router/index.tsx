51e99dbe45f7850c3d854aeaa6074010172e8ca6
jeonsumin <soomis7@naver.com>
2024-07-25 14:46:55 +0900

    add router current setup

---
+++ src/router/index.tsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from "react-router-dom";
+import About from "views/About";
+import Main from "views/Main";
+import Root from "views/Root";
+
+export const router = createBrowserRouter([
+    {
+        path:"/",
+        element: <Root />,
+        children: [ // Root.tsx에 Outlet으로 출력
+            { 
+                path: "", 
+                element: <Main/> 
+            },
+            { 
+                path: "about", 
+                element: <About/> 
+            },
+        ],
+    },
+    // {
+    //     path:"/login",
+    //     element:<Login/>,
+    // }
+])
